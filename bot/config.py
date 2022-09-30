87abd89d879a7003ba6b22388ac16bd8425c75d7
erickyun <71968711+erickyun@users.noreply.github.com>
2022-09-30 13:55:55 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,5 @@
-#    Copyright (c) 2021 Danish_00
-#    Improved By @Zylern
+#    Copyright (c) 2021 
+#    Improved By @
 
 from decouple import config
 
@@ -7,10 +7,10 @@ try:
     APP_ID = config("APP_ID", cast=int)
     API_HASH = config("API_HASH")
     BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1664850827
+    DEV = 0
     OWNER = config("OWNER")
-    ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
+    ffmpegcode = ["-preset slower -c:v libx265 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By 9LifeCat' -pix_fmt yuv420p10 -crf 16 -c:a aac -b:a 128k -c:s copy"]
+    THUMBNAIL = config("THUMBNAIL", default="https://trac.ffmpeg.org/ffmpeg-logo.png")
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
     print(str(e))
